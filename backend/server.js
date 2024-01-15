22d8fa13bebebd6eac4ae0d8c29ee3b39912cc84
ItzAnsh <anshbhasin3723@gmail.com>
2024-01-15 20:47:48 +0530

    Cors added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 const connectDB = require("./config/connectDB/connectDB");
 
@@ -9,7 +11,7 @@ const studentRoute = require("./routes/student/route");
 const teacherRoute = require("./routes/teacher/route");
 const courseRoute = require("./routes/course/route");
 const adminRoute = require("./routes/admin/route");
-const homeRoute = require('./routes/public/route');
+const homeRoute = require("./routes/public/route");
 
 const PORT = 5001;
 
@@ -18,7 +20,7 @@ app.use("/api/student", studentRoute);
 app.use("/api/teacher", teacherRoute);
 app.use("/api/course", courseRoute);
 app.use("/api/admin", adminRoute);
-app.use("/api", homeRoute)
+app.use("/api", homeRoute);
 
 app.listen(PORT, () => {
 	console.log(`Server running at ${PORT}`);
