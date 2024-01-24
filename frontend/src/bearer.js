44f4f57302fdc6970cad257384b6005115c2618d
Hiti Dudeja <dudejahiti11@gmail.com>
2024-01-24 16:34:01 +0530

    Some changes

--- frontend/src/bearer.js
+++ frontend/src/bearer.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const bearerUrl = "http://lms-backend.vercel.app"; // Update this URL to the correct server URL
+const bearerUrl = "https://lms-seven-delta.vercel.app"; // Update this URL to the correct server URL
 
 const publicApi = {
 	get: async (url) => {
