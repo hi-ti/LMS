59f364c8b0b17526363d7cbd32595e5c3f2e5dbf
ItzAnsh <anshbhasin3723@gmail.com>
2024-01-25 09:54:46 +0530

    All orgiins

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors());
+app.use(
+	cors({
+		origin: "*",
+	})
+);
 app.use(express.json());
 const connectDB = require("./config/connectDB/connectDB");
 
